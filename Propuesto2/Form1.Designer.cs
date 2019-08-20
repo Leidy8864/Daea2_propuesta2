ccf1efe0069c9535a8fd19a1e725c8058038d4ac
Leidy C_S <leidy.callupe@tecsup.edu.pe>
2019-08-19 20:43:29 -0500

    first commit

---
+++ Propuesto2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Propuesto2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgPedidos = new System.Windows.Forms.DataGridView();
+            this.dgDetalles = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPedidos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDetalles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(169, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lista de pedidos";
+            // 
+            // dgPedidos
+            // 
+            this.dgPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPedidos.Location = new System.Drawing.Point(68, 52);
+            this.dgPedidos.Name = "dgPedidos";
+            this.dgPedidos.Size = new System.Drawing.Size(279, 363);
+            this.dgPedidos.TabIndex = 1;
+            this.dgPedidos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgPedidos_CellClick_1);
+            this.dgPedidos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgPedidos_CellContentClick);
+            // 
+            // dgDetalles
+            // 
+            this.dgDetalles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDetalles.Location = new System.Drawing.Point(395, 52);
+            this.dgDetalles.Name = "dgDetalles";
+            this.dgDetalles.Size = new System.Drawing.Size(240, 363);
+            this.dgDetalles.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(488, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Productos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgDetalles);
+            this.Controls.Add(this.dgPedidos);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgPedidos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDetalles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgPedidos;
+        private System.Windows.Forms.DataGridView dgDetalles;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Propuesto2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Propuesto2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dgPedidos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["kobu"].ConnectionString);
+
+        public void ListaPedidos()
+        {
+            using (SqlDataAdapter df = new SqlDataAdapter("Usp_Pedidos", cn))
+            {
+                df.SelectCommand.CommandType = CommandType.StoredProcedure;
+                using (DataSet da = new DataSet())
+                {
+                    df.Fill(da, "Pedidos");
+                    dgPedidos.DataSource = da.Tables["Pedidos"];
+
+                }
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            ListaPedidos();
+
+        }
+
+        private void dgPedidos_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            int Codigo;
+            Codigo = Convert.ToInt32(dgPedidos.CurrentRow.Cells[0].Value);
+            using (SqlCommand cmd = new SqlCommand("Usp_Detalle_Pedido", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    da.SelectCommand.Parameters.AddWithValue("@idpedido", Codigo);
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "Detalles");
+                        dgDetalles.DataSource = df.Tables["Detalles"];
+                    }
+                }
+            }
+        }
+    }
+}
